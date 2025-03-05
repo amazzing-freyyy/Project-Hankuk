79fe24216c5737de864b6fca2846d63d0cd757e3
Alejandro Fabián González Pacheco <71179997+amazzing-freyyy@users.noreply.github.com>
2025-03-05 02:11:48 +0100

    Update forms.py

--- team_dashboard/forms.py
+++ team_dashboard/forms.py
@@ -29,7 +29,7 @@ class WakeUpForm(forms.Form):
     HR= forms.DecimalField(max_digits=10, decimal_places=2, widget=forms.TextInput(attrs={'class': 'form-control'}), error_messages={'invalid':'Valor de HR inválido'}, required=False)
     emotional_wellness= forms.IntegerField(widget=StarRating(attrs={'class':'star-rating'}, max_stars=5, tags=['Motivado', 'Normal', 'Desmotivado']), error_messages={'required':'Hace falta el ánimo','invalid':'Valor de ánimo inválido'} ,)
     chispa= forms.IntegerField(widget=StarRating(attrs={'class':'star-rating'}, max_stars=5, tags=['Mucha', 'Moderada', 'Nada']), error_messages={'required':'Hace falta la chispa','invalid':'Valor de chispa inválido'} ,)
-    hours_of_sleep= TimeMultiField(error_messages={'required':'Hace falta un valor en las horas de sueño'})
+    hours_of_sleep= TimeMultiField(error_messages={'required':'Hace falta un valor en las horas de sueño', 'invalid':'Las horas de sueño están mal escritas.(solo números)'})
     quality_of_sleep= forms.IntegerField(widget=StarRating(attrs={'class':'star-rating'}, max_stars=5, tags=['Muy Buena', 'Buena', 'Problemas al dormir', 'Muy Mala', 'No dormí']), error_messages={'required':'Hace falta la calidad de sueño.', 'invalid':'Valor de calidad de sueño inválido'} ,)
     muscle_pain= forms.IntegerField(widget=StarRating(attrs={'class':'star-rating'}, max_stars=5, tags=['Nada', 'Bajo', 'Normal', 'Dolorido', 'Muy Dolorido']), error_messages={'required':'Hace falta saber cuanto te duele.', 'invalid':'Valor de dolor inválido'} ,)
     tiredness= forms.IntegerField(widget=StarRating(attrs={'class':'star-rating'}, max_stars=5, tags=['Recuperado', 'Normal', 'Fatigado']), error_messages={'required':'Hace falta saber cuanto haz recuperado.', 'inválid':'Valor de Cansancio inválido'} ,)
@@ -38,9 +38,9 @@ class WakeUpForm(forms.Form):
     comments= forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}), required=False)
 
 class PostTrainingForm(forms.Form):
-    date= DateTimeField(error_messages={'required':'Hace falta la fecha'})
+    date= DateTimeField(error_messages={'required':'Hace falta la fecha', 'invalid':'La fecha está mal escrita. (es más fácil usar los botones)'})
     type_of_activity= TrainingField(required=False, error_messages={'required':'Hace falta el tipo de actividad'} )
-    time_of_activity= forms.DecimalField(widget=forms.TextInput(attrs={'class': 'form-control'}), error_messages={'invalid':'Valor de tiempo inválido.','required':'Hace falta la duración de la actividad'})
+    time_of_activity= forms.DecimalField(widget=forms.TextInput(attrs={'class': 'form-control'}), error_messages={'invalid':'Valor de tiempo inválido.(solo números)','required':'Hace falta la tiempo de la actividad'})
     perceived_strain_of_activity= forms.IntegerField(widget=StarRating(attrs={'class':'star-rating'}, max_stars=10, tags=['Max. Esfuerzo', 'Moderado', 'Leve']), error_messages={'invalid':'Valor de esfuerzo inválido.','required':'Hace falta el valor de esfuerzo'}, required=True)
     pain= forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), required=False)
     comments= forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), required=False)
