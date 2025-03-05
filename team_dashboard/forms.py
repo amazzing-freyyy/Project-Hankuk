9cff754404aebb51d8d71d7fa9e364ca985f4960
Alejandro Fabián González Pacheco <alelnn2000@gmail.com>
2025-03-05 14:26:18 +0100

    inputmode bug fix

--- team_dashboard/forms.py
+++ team_dashboard/forms.py
@@ -29,7 +29,7 @@ class WakeUpForm(forms.Form):
     HR= forms.DecimalField(max_digits=10, decimal_places=2, widget=forms.TextInput(attrs={'class': 'form-control', 'inputmode':'numeric'}), error_messages={'invalid':'Valor de HR inválido'}, required=False)
     emotional_wellness= forms.IntegerField(widget=StarRating(attrs={'class':'star-rating'}, max_stars=5, tags=['Motivado', 'Normal', 'Desmotivado']), error_messages={'required':'Hace falta el ánimo','invalid':'Valor de ánimo inválido'} ,)
     chispa= forms.IntegerField(widget=StarRating(attrs={'class':'star-rating'}, max_stars=5, tags=['Mucha', 'Moderada', 'Nada']), error_messages={'required':'Hace falta la chispa','invalid':'Valor de chispa inválido'} ,)
-    hours_of_sleep= TimeMultiField(attrs={'inputmode':'numeric'}, error_messages={'required':'Hace falta un valor en las horas de sueño'})
+    hours_of_sleep= TimeMultiField(error_messages={'required':'Hace falta un valor en las horas de sueño'})
     quality_of_sleep= forms.IntegerField(widget=StarRating(attrs={'class':'star-rating'}, max_stars=5, tags=['Muy Buena', 'Buena', 'Problemas al dormir', 'Muy Mala', 'No dormí']), error_messages={'required':'Hace falta la calidad de sueño.', 'invalid':'Valor de calidad de sueño inválido'} ,)
     muscle_pain= forms.IntegerField(widget=StarRating(attrs={'class':'star-rating'}, max_stars=5, tags=['Nada', 'Bajo', 'Normal', 'Dolorido', 'Muy Dolorido']), error_messages={'required':'Hace falta saber cuanto te duele.', 'invalid':'Valor de dolor inválido'} ,)
     tiredness= forms.IntegerField(widget=StarRating(attrs={'class':'star-rating'}, max_stars=5, tags=['Recuperado', 'Normal', 'Fatigado']), error_messages={'required':'Hace falta saber cuanto haz recuperado.', 'inválid':'Valor de Cansancio inválido'} ,)
