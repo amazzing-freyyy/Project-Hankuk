b03b75433db2459933e5ec8240532076885893f9
Alejandro Fabián González Pacheco <alelnn2000@gmail.com>
2025-03-13 09:11:20 +0100

    added date to report

--- team_dashboard/views.py
+++ team_dashboard/views.py
@@ -145,6 +145,7 @@ class Wellness_Dashboard(LoginRequiredMixin, TemplateView):
             autosize=True,
             dragmode= 'pan',
             hovermode='closest',
+            title= f'Fecha: {start_date.strftime("%m/%d/%Y")}'
         )
 
         lnrmssd_trace= go.Scatter(
