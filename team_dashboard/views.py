359047699fc39d7e382b8f4ec83cb9e6c732dee6
Alejandro Fabián González Pacheco <alelnn2000@gmail.com>
2025-05-09 22:58:12 +0200

    title bug fix pt view

--- team_dashboard/views.py
+++ team_dashboard/views.py
@@ -163,7 +163,7 @@ class Wellness_Dashboard(LoginRequiredMixin, TemplateView):
             autosize=True,
             dragmode= 'pan',
             hovermode='closest',
-            title= f'Fecha: {graph_data.first()['date'].strftime("%m/%d/%Y")}',
+            title= f'Fecha: {graph_data.last()['date'].strftime("%m/%d/%Y")}',
         )
 
         lnrmssd_trace= go.Scatter(
