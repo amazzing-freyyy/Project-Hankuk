5496fba94cc13f5185cd54605ff4c3980fea388c
Alejandro Fabián González Pacheco <alelnn2000@gmail.com>
2025-10-05 00:25:16 +0200

    took out logging

--- team_dashboard/serializers.py
+++ team_dashboard/serializers.py
@@ -48,7 +48,6 @@ class UserSerializer(serializers.ModelSerializer):
         fields = [ 'username', 'password', 'first_name', 'last_name', 'email', 'is_active', 'groups', 'profile']
 
     def update(self, instance, validated_data):
-        logger.error("Validated data: {validated_data}")
         password = validated_data.pop('password', None)
         profile_data = validated_data.pop('profile', None)
 
