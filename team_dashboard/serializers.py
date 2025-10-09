507ea240ecba94bc403c474801282e489a4893a3
Alejandro Fabián González Pacheco <alelnn2000@gmail.com>
2025-10-09 21:34:36 +0200

    added avatar to profile serializer

--- team_dashboard/serializers.py
+++ team_dashboard/serializers.py
@@ -32,7 +32,7 @@ class PTDSerializer(serializers.ModelSerializer):
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['gender']
+        fields = ['__all__']
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=False)
