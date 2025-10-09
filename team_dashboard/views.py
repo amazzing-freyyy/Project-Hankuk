49ed70f3de513f8178b8a6a979a90b7d12ee9048
Alejandro Fabián González Pacheco <alelnn2000@gmail.com>
2025-10-09 13:28:23 +0200

    bug fix

--- team_dashboard/views.py
+++ team_dashboard/views.py
@@ -55,7 +55,8 @@ class UserViewSet(ModelViewSet):
     
     def get_permissions(self):
         if self.action in ['update', 'partial_update', 'destroy']:
-            return [IsAuthenticated(), (IsOwner() | IsAdmin())]
+
+            return [IsAuthenticated(), IsOwner | IsAdmin]
         return [AllowAny()]
 
 class AthleteViewSet(UserViewSet):
