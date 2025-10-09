39c3c7595fe0aeb5421559f826e5158a8712c1ef
Alejandro Fabián González Pacheco <alelnn2000@gmail.com>
2025-10-09 14:58:42 +0200

    new change password url

--- team_dashboard/views.py
+++ team_dashboard/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User, Group
 import logging
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, action
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.viewsets import ModelViewSet
@@ -52,13 +52,38 @@ class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     lookup_field = 'username'
     filter_backends = [DjangoFilterBackend]
-    
+
     def get_permissions(self):
         if self.action in ['update', 'partial_update', 'destroy']:
-
             return [IsAuthenticated(), IsAdminOrOwner()]
         return [AllowAny()]
 
+    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated, IsOwner | IsAdmin])
+    def change_password(self, request, username=None):
+        user = self.get_object()
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+        conf_password = request.data.get("conf_password")
+
+        if not new_password or not conf_password:
+            return Response({"detail": "Both new_password and conf_password are required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        if new_password != conf_password:
+            return Response({"detail": "Passwords do not match."}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not request.user.is_staff:
+            if not old_password or not user.check_password(old_password):
+                return Response({"detail": "Old password is incorrect."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            validate_password(new_password, user)
+        except ValidationError as e:
+            return Response({"detail": list(e.messages)}, status=status.HTTP_400_BAD_REQUEST)
+
+        user.set_password(new_password)
+        user.save()
+        return Response({"detail": "Password changed successfully."}, status=status.HTTP_200_OK)
+    
 class AthleteViewSet(UserViewSet):
     queryset = User.objects.filter(groups__name='athletes')
     serializer_class = UserSerializer
