3c86492d001e97a17743f574c987d8824c0301e8
Alejandro Fabián González Pacheco <alelnn2000@gmail.com>
2025-10-09 18:14:45 +0200

    order by ss date

--- team_dashboard/views.py
+++ team_dashboard/views.py
@@ -174,7 +174,7 @@ def get_ssData(request,username):
     else:
         return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
-    query = Main_data.objects.filter(user=user, data_collection='wellness').values('date', 'data').all().order_by('-date')
+    query = Main_data.objects.filter(user=user, data_collection='wellness').values('date', 'data').all().order_by('date')
 
     if not query.exists():
         return Response({'error': 'No data'}, status=status.HTTP_404_NOT_FOUND)
